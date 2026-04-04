bd7f344200761515425c26c4b5ca5dfca7922c8f
MaksimBoyko05 <vladosmakasin1@gmail.com>
2026-04-04 15:15:34 +0300

    fix: add imports & space under code for pipeline

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 from sklearn.datasets import load_iris
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
-from sklearn.metrics import classification_report, confusion_matrix
-import seaborn as sns
-import matplotlib.pyplot as plt
+from sklearn.metrics import classification_report
 
 iris = load_iris()
-X_train, X_test, y_train, y_test = train_test_split(iris.data, iris.target, test_size=0.2, random_state=42)
+X_train, X_test, y_train, y_test = train_test_split(
+    iris.data, iris.target, test_size=0.2, random_state=42
+)
 model = RandomForestClassifier(n_estimators=100, random_state=42)
 model.fit(X_train, y_train)
 predictions = model.predict(X_test)
